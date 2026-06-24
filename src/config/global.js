dd9563ade07eb9400b9075becdc522231daa773a
SENA <cfmartinezs@hotmail.com>
2026-06-23 21:04:11 -0500

    repo

--- src/config/global.js
+++ src/config/global.js
@@ -243,7 +243,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/123103_CF09_DU.pdf',
       },
       {
         icono: 'fas fa-download',
